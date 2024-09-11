2d32143b137190ce5db1c265aa8d4ed1b6fc1440
amaliehans@uia.no <amaliehans@uia.no>
2024-09-11 11:16:43 +0200

    opp 2 1

--- module_2/task_2-2/task_2-2.mjs
+++ module_2/task_2-2/task_2-2.mjs
@@ -4,17 +4,35 @@ initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("Original expression: 2 + 3 * 2 - 4 * 6");
+const calculation = 2 + 3 * (2 - 4) * 6;
+printOut("Svar " + calculation);
 printOut(newLine);
 
-printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
+print("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("Konverter 25 m og 34 cm til inches");
+const antallMeter = 25;
+const antallCM = 34;
+let meterInMillimeter = antallMeter * 1000;
+printOut("Meter i mm " + meterInMillimeter);
+let CMInMillimeter = antallCM * 10;
+printOut("CM i mm " + CMInMillimeter);
+let antallMM = meterInMillimeter + CMInMillimeter;
+printOut("Total antall MM " + antallMM);
+let MMtoinches = antallMM / 25.4;
+printOut("Antall Inches " + MMtoinches.toFixed(2));
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("Konverter 3 dager, 12 t, 4 min og 45 sek til min");
+const antallDager = 3;
+const antallTimer = 12;
+const antallMin = 4;
+const antallSek = 45;
+let antallDager1 = antallMin * 60;
+printOut("Dagertilmin" + antalldager1);
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
