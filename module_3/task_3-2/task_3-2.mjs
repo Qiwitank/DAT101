ff8a50ddb3bc7cc0a11b97fee53428780a3a4462
amaliehans@uia.no <amaliehans@uia.no>
2024-11-06 21:15:39 +0100

    task 3233

--- module_3/task_3-2/task_3-2.mjs
+++ module_3/task_3-2/task_3-2.mjs
@@ -4,51 +4,179 @@ initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+let output = '';
+for (let i = 1; i <= 10; i++) output += i + ' ';
+output += '<br>';
+for (let i = 10; i >= 1; i--) output += i + ' ';
+printOut(output.toString());
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+const targetNumber = 45
+let guessedNumber;
+
+while (true){
+    guessedNumber = Math.floor(Math.random() * 60) + 1;
+    if (guessedNumber === targetNumber) {
+        break;
+    }
+}
+printOut("The guessed number is: " + guessedNumber.toString());
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+const targetNumber2 = Math.floor(Math.random() * 1000000) + 1;
+let guessedNumber2;
+let guesses = 0;
+const startTime = Date.now();
+while (true){
+    guessedNumber2 = Math.floor(Math.random() * 1000000) + 1;
+    guesses++;
+    if (guessedNumber2 === targetNumber2) {
+        break;
+    }
+}
+const endTime = Date.now();
+const timeTaken = endTime - startTime;
+printOut("The number is: " + guessedNumber2.toString());
+printOut("How many guesses it took: " + guesses.toString());
+printOut("How much time it took: " + timeTaken.toString());
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+function findPrimes(){
+    const primes = [];
+    for (let num = 2; num < 200; num++) {
+        let isPrime = true;
+        let i = 2;
+        while (i * i <= num) {
+            if (num % i === 0){
+                isPrime = false;
+                break;
+
+            }
+            i++;
+        }
+        
+        if (isPrime) {
+            primes.push(num);
+        }
+    }
+
+    return primes;
+
+    }
+
+
+const primeNumbers = findPrimes();
+printOut(primeNumbers.toString());
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+for (let row = 1; row <= 7; row++){
+    let rowText = "";
+    for (let column = 1; column <= 9; column++){
+        rowText += "K" + column + "R" + row + " ";
+    }
+    printOut(rowText);
+}
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const points = Math.ceil(Math.random() * 236);
+const percent = points / 236 * 100;
+let grade;
+if (percent < 40) {
+    grade = "F";
+} else if (percent < 52) {
+    grade = "E";
+}else if (percent < 64) {
+    grade = "D";
+}else if (percent < 76) {
+    grade = "C";
+}else if (percent < 88) {
+    grade = "B";
+}else {
+    grade = "A";
+}
+
+printOut("Studenten fikk " + grade.toString() + " og fikk " + points.toString() + " poeng.");
+
+
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
 
-printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
-printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
-/* Task 10*/
-printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+let answer = "";
+  let rolls = 0;
+  let done = false;
+  let hasYahtzee = false;   // True if we have already got Yahtzee
+  let hasStraight = false;  // True if we have already got Straight
+  let has3Pair = false;     // True if we have already got 3 pair
+  let hasTower = false;     // True if we have already got four and two equals
+  do {
+    const d1 = Math.ceil(Math.random() * 6);  // Dice one
+    const d2 = Math.ceil(Math.random() * 6);  // Dice two
+    const d3 = Math.ceil(Math.random() * 6);  // Dice three
+    const d4 = Math.ceil(Math.random() * 6);  // Dice four
+    const d5 = Math.ceil(Math.random() * 6);  // Dice five
+    const d6 = Math.ceil(Math.random() * 6);  // Dice six
+    const txtD = (d1 + "," + d2 + "," + d3 + "," + d4 + "," + d5 + "," + d6); // make string of all dice's
+    rolls++;  // increase rolls with one, computer has rolled all dices.
+    // Check to see if all dice are equal (Yahtzee!)
+    if ((d1 === d2) && (d1 === d3) && (d1 === d4) && (d1 === d5) && (d1 === d6)) {
+      // All dice are equal, check if we already has got Yahtzee
+      if (hasYahtzee === false) {
+        hasYahtzee = true;
+        answer += txtD + newLine;
+        answer += "Yatzi!" + newLine;
+        answer += "På " + rolls.toString() + " kast!" + newLine + newLine;
+      }
+    } else {
+      // All dice are not equal, count each number of occurrence
+      // Find match for each 1,2,3,4,5,6, if no match found use a empty string so length will work.
+      
+      const cD1 = (txtD.match(/1/g) || "").length; //count1
+      const cD2 = (txtD.match(/2/g) || "").length;
+      const cD3 = (txtD.match(/3/g) || "").length;
+      const cD4 = (txtD.match(/4/g) || "").length;
+      const cD5 = (txtD.match(/5/g) || "").length;
+      const cD6 = (txtD.match(/6/g) || "").length;
+      if ((cD1 === 1) && (cD2 === 1) && (cD3 === 1) && (cD4 === 1) && (cD5 === 1) && (cD6 === 1)) {
+        // All dice are different, check if we already got Straight
+        if (hasStraight === false) {
+          hasStraight = true;
+          answer += txtD + newLine;
+          answer += "Full straight" + newLine;
+          answer += "På " + rolls.toString() + " Kast!" + newLine + newLine;
+        }
+      } else {
+        // we have neither Yahtzee or Straight
+        // Count number of equal occurrence
+        const txtCD = cD1 + "," + cD2 + "," + cD3 + "," + cD4 + "," + cD5 + "," + cD6;
+        const ccD2 = (txtCD.match(/2/g) || "").length;
+        const ccD4 = (txtCD.match(/4/g) || "").length;
+        if (ccD2 === 3) {
+          // Check if we already got 3 pair
+          if (has3Pair === false) {
+            has3Pair = true;
+            answer += txtD + newLine;
+            answer += "3 Par" + newLine;
+            answer += "På " + rolls.toString() + " kast!" + newLine  + newLine;
+          }
+        } else if ((ccD2 === 1) && (ccD4 === 1)) {
+          // Check if we already got tower
+          if (hasTower === false) {
+            hasTower = true;
+            answer += txtD + newLine + newLine;
+            answer += "Tårn" + newLine;
+            answer += "På " + rolls.toString() + " kast!" + newLine  + newLine;
+          }
+        }
+      }
+    }
+    done = hasYahtzee && hasStraight;
+  } while (done === false);
+  printOut(answer + newLine);
--- module_3/task_3-3/task_3-3.mjs
+++ module_3/task_3-3/task_3-3.mjs
@@ -4,47 +4,234 @@ initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+function dateFinder(){
+    const date = new Date();
+printOut(date.toLocaleDateString('no-NB', {
+    dateStyle: 'full'}));
+    return date  
+    }dateFinder();
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const theDateToday = dateFinder();
+
+function theAmountOfDays(finalDay, endTxt){
+    const dateTill = new Date(finalDay);
+    const remaindingDays = dateTill - theDateToday;
+
+    if(remaindingDays < 0) {
+        printOut("The date has alredy passed");
+        return;
+    }
+
+    const days = Math.floor(remaindingDays / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((remaindingDays % (1000 * 60 * 60 * 24)) /(1000 * 60 * 60))
+    const minutes = Math.floor((remaindingDays % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((remaindingDays % (1000 * 60)) / 1000);
+
+    printOut(days + "d " + hours + "h " + minutes + "m " + seconds + "s " + endTxt);
+}
+theAmountOfDays("2025-05-14", "until the release of PS5");
+
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+function calculateCircleProperties(radius) {
+    const diameter = 2 * radius;
+    const circumference = 2 * Math.PI * radius;
+    const area = Math.PI * Math.pow(radius, 2);
+
+    printOut("For a circle with radius: " + radius);
+    printOut("Diameter: " + diameter);
+    printOut("Circumference: " + circumference.toFixed(2));
+    printOut("Area: " + area.toFixed(2));
+}
+calculateCircleProperties(5);
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+function rectangleProperties(dimensions) {
+    const width = dimensions.width;
+    const height = dimensions.height;
+    const circumference = 2 * (width + height);
+    const area = width * height
+    printOut("circumference (Perimeter): ", circumference);
+    printOut("area: ", area);
+}
+rectangleProperties({ width: 10, height: 5 });
+
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const ETemperatureType = { Celsius: 1, Fahrenheit: 2, Kelvin: 3};
+
+function convertTemperature(aTemperature, aType) {
+    let Fahrenheit = 0;
+    let Celsius = 0;
+    let Kelvin = 0;
+    switch (aType) {
+        case ETemperatureType.Celsius:
+            printOut("Convert from Celsius");
+            //convert to Fahrenheit
+            //Fahrenheit = (Kelvin - 237.15)*9/5 + 32;
+            Celsius = aTemperature;
+            Fahrenheit = (Celsius * 9) / 5 + 32;
+            Kelvin = Celsius + 237.15;
+            break;
+        case ETemperatureType.Fahreinheit:
+            printOut("Convert from Fahreinheit");
+            break;
+        case ETemperatureType.Kelvin:
+            printOut("Convert from Kelvin");
+            break;
+    } // End switch
+
+    printOut("Celsius = " + Celsius.toFixed(0));
+    printOut("Fahrenheit = " + Fahrenheit.toFixed(0))
+    printOut("Kelvin = " + Kelvin.toFixed(0));
+} // End function
+
+convertTemperature(37.5, ETemperatureType.Celsius);
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+printOut("");
+
+function calculateNetPrice(aPrice, aTaxGroup) {
+  let net = NaN;
+  let taxGroup = aTaxGroup.toUpperCase();
+  let vat = NaN;
+
+  printOut("taxGroup = " + taxGroup);
+
+  switch (taxGroup) {
+    case "NORMAL":
+      vat = 25;
+  }
+
+  if (!Number.isNaN(vat)) {
+    net = (100 * aPrice) / (vat + 100);
+  }
+
+  return net;
+}
+
+const netPrice1 = calculateNetPrice(100, "normal");
+if (Number.isNaN(netPrice1)) {
+  printOut("Unknown VAT group!");
+} else {
+  printOut("netPrice1 = " + netPrice1.toFixed(2));
+}
+
+const netPrice2 = calculateNetPrice(100, "goblins");
+if (Number.isNaN(netPrice1)) {
+  printOut("Unknown VAT group!");
+} else {
+  printOut("netPrice2 = " + netPrice2.toFixed(2));
+}
+
+printOut("");
+
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+function calculateSpeedDistanceTime(distance, time, speed) {
+    const missingCount = [distance, time, speed].filter(val => val === undefined).length;
+    if (missingCount !== 1) {
+        return NaN; 
+    }
+    if (speed === undefined) {
+        const calculatedSpeed = distance / time;  
+        printOut("Calculated Speed: " + calculatedSpeed);
+        return calculatedSpeed;
+    } else if (time === undefined) {
+        const calculatedTime = distance / speed;  
+        printOut("Calculated Time: " + calculatedTime);
+        return calculatedTime;
+    } else if (distance === undefined) {
+        const calculatedDistance = speed * time;  
+        printOut("Calculated Distance: " + calculatedDistance);
+        return calculatedDistance;
+    }
+}
+calculateSpeedDistanceTime(100, 2, undefined);
+calculateSpeedDistanceTime(100, undefined, 50); 
+calculateSpeedDistanceTime(undefined, 2, 50); 
+calculateSpeedDistanceTime(100, undefined, undefined); 
+
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+function testIfMathIsFun() {
+    let op = 1;
+    let line = 1;
+    // Left side
+    let ok = false;
+    do {
+      let sumLeft = 0;
+      for (let left = 0; left < line + 1; left++) {
+        sumLeft += op;
+        op++;
+      }
+  
+      let sumRight = 0;
+      for (let right = 0; right < line; right++) {
+        sumRight += op;
+        op++;
+      }
+  
+      if (sumLeft !== sumRight) {
+        ok = false;
+        printOut("Error in line " + line.toString());
+      }else{
+        ok = true;
+      }
+      line++;
+  
+      if(line > 200){
+        printOut("Math is Fun!");
+        break;
+      }
+      
+    } while (ok);
+  }
+  
+  testIfMathIsFun();
+  
+  
+  printOut(" ");
+  /*
+    Use recursion to count from 1 to 10 and then back to 1 again. 
+  */
+  const countTo = 10;
+  function count(aNumber) {
+    if (aNumber <= countTo) {
+      printOut(aNumber.toString());
+      count(aNumber + 1);
+      printOut(aNumber.toString());
+    }
+  }
+  
+  count(1);
+
 printOut(newLine);
 
 /* Task 10*/
