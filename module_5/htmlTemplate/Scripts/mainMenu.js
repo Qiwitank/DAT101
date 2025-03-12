d3fe5b31a9588993d1d54f97f4bfb2f1dd585fef
amaliehans@uia.no <amaliehans@uia.no>
2025-03-12 12:09:37 +0100

    html

---
+++ module_5/htmlTemplate/Scripts/mainMenu.js
@@ -0,0 +1,29 @@
+"use strict";
+import TBootstrapComponent from "./bootstrapComponent.js";
+
+class TMainMenu extends TBootstrapComponent {
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"});
+    }
+
+    render(){
+        const template = document.getElementById("main-menu-template");
+        const content = template.content.cloneNode(true);
+        this.shadowRoot.appendChild(content);
+        this.#createMenuManager();
+    }
+
+    #createMenuManager(){
+        const menuItems = this.shadowRoot.querySelector("a[page-data]");
+        for(let i = 0; i < menuItems.length; i++){
+            const menuItem = menuItems[i];
+            menuItem.addEventListener("click",
+            () => {
+                console.log("Navigation to " + menuItem.dataset.page);
+            });
+        }
+    }
+}
+
+customElements.define("main-menu", TMainMenu);
